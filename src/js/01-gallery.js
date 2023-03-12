313567072433605b8cb0bbddff15275010fe7b2a
Олександр <moskaletzsanych@gmail.com>
2023-03-12 16:59:39 +0200

    task-1-is-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainerEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGalleryItemsMarkup = array => {
+  return array
+    .map(({ preview, original, description }) => {
+      const src = preview;
+      const source = original;
+      const alt = description;
+
+      return `<div class="gallery__item">
+    <a class="gallery__item" href=${source}>
+    <img
+    class="gallery__image"
+    src=${src}
+    alt=${alt}
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+};
+
+const markup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('afterbegin', markup);
+
+let lightbox;
+
+const handleGalleryItemClick = event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+  });
+};
+
+galleryContainerEl.addEventListener('click', handleGalleryItemClick);
