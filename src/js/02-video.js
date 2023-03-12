7065a2a3f9de01d54d4c35154b1e148b102dd844
Олександр <moskaletzsanych@gmail.com>
2023-03-12 21:27:08 +0200

    task-2-is-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENTTIME = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+const setItemToLocalStorage = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(setItemToLocalStorage, 1000));
+
+if (CURRENTTIME) {
+  player.setCurrentTime(CURRENTTIME);
+}
